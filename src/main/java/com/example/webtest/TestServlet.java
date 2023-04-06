7d51adf2950bdffc44765b87e000f4b534e3aefa
yanqi17118357 <3224848830@qq.com>
2023-04-06 22:40:28 +0800

    HttpServlet使用

--- src/main/java/com/example/webtest/TestServlet.java
+++ src/main/java/com/example/webtest/TestServlet.java
@@ -2,43 +2,23 @@ package com.example.webtest;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.java.Log;
 
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.HashMap;
 
 @Log
 @WebServlet("/test")
-public class TestServlet implements Servlet {
+public class TestServlet extends HttpServlet {
 
     @Override
-    public void init(ServletConfig servletConfig) throws ServletException {
-    }
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-    @Override
-    public ServletConfig getServletConfig() {
-        return null;
-    }
-
-    @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        //转换为HttpServletResponse（同上）
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        //设定内容类型以及编码格式（普通HTML文本使用text/html，之后会讲解文件传输）
-        response.setHeader("Content-type", "text/html;charset=UTF-8");
-        //获取Writer直接写入内容
-        response.getWriter().write("我是响应内容！");
-        //所有内容写入完成之后，再发送给浏览器
-    }
-
-    @Override
-    public String getServletInfo() {
-        return null;
-    }
-
-    @Override
-    public void destroy() {
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.getWriter().write("<h1>恭喜你解锁了全新玩法</h1>");
     }
 }
