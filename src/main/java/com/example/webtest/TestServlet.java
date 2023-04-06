802bea15f6126f86dad04101a54f6d9de9567150
yanqi17118357 <3224848830@qq.com>
2023-04-06 19:26:42 +0800

    ServletRequest中的信息

--- src/main/java/com/example/webtest/TestServlet.java
+++ src/main/java/com/example/webtest/TestServlet.java
@@ -2,42 +2,47 @@ package com.example.webtest;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.java.Log;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 @Log
 @WebServlet("/test")
 public class TestServlet implements Servlet {
 
-    public TestServlet(){
-        System.out.println("我是构造方法！");
-    }
-
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
-        System.out.println("我是init");
     }
 
     @Override
     public ServletConfig getServletConfig() {
-        System.out.println("我是getServletConfig");
         return null;
     }
 
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println("我是service");
+        //首先将其转换为HttpServletRequest（继承自ServletRequest，一般是此接口实现）
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        System.out.println(request.getProtocol());  //获取协议版本
+        System.out.println(request.getRemoteAddr());  //获取访问者的IP地址
+        System.out.println(request.getMethod());   //获取请求方法
+        //获取头部信息
+        Enumeration<String> enumeration = request.getHeaderNames();
+        while (enumeration.hasMoreElements()){
+            String name = enumeration.nextElement();
+            System.out.println(name + ": " + request.getHeader(name));
+        }
     }
 
     @Override
     public String getServletInfo() {
-        System.out.println("我是getServletInfo");
         return null;
     }
 
     @Override
     public void destroy() {
-        System.out.println("我是destroy");
     }
 }
