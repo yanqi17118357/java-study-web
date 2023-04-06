0d1206192117a205c8475aca9a6edf4033e4ad7a
yanqi17118357 <3224848830@qq.com>
2023-04-06 21:55:14 +0800

    ServletResponse返回响应信息

--- src/main/java/com/example/webtest/TestServlet.java
+++ src/main/java/com/example/webtest/TestServlet.java
@@ -3,6 +3,7 @@ package com.example.webtest;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.java.Log;
 
 import java.io.IOException;
@@ -23,18 +24,13 @@ public class TestServlet implements Servlet {
 
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        //首先将其转换为HttpServletRequest（继承自ServletRequest，一般是此接口实现）
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-
-        System.out.println(request.getProtocol());  //获取协议版本
-        System.out.println(request.getRemoteAddr());  //获取访问者的IP地址
-        System.out.println(request.getMethod());   //获取请求方法
-        //获取头部信息
-        Enumeration<String> enumeration = request.getHeaderNames();
-        while (enumeration.hasMoreElements()){
-            String name = enumeration.nextElement();
-            System.out.println(name + ": " + request.getHeader(name));
-        }
+        //转换为HttpServletResponse（同上）
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        //设定内容类型以及编码格式（普通HTML文本使用text/html，之后会讲解文件传输）
+        response.setHeader("Content-type", "text/html;charset=UTF-8");
+        //获取Writer直接写入内容
+        response.getWriter().write("我是响应内容！");
+        //所有内容写入完成之后，再发送给浏览器
     }
 
     @Override
