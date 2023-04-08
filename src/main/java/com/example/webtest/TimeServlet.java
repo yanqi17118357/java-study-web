91aae948ec6b93430ec6633dc0059084ebd2ea0a
yanqi17118357 <3224848830@qq.com>
2023-04-08 22:26:06 +0800

    Servlet应用-前端xhr请求数据

---
+++ src/main/java/com/example/webtest/TimeServlet.java
@@ -0,0 +1,23 @@
+package com.example.webtest;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet("/time")
+public class TimeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        String date = dateFormat.format(new Date());
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.getWriter().write(date);
+    }
+}
