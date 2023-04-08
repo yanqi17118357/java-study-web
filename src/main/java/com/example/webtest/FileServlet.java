a2997334d6b3d4b5f4da19ace4e1bb05d81197a3
yanqi17118357 <3224848830@qq.com>
2023-04-08 22:12:48 +0800

    Servlet应用-上传文件

--- src/main/java/com/example/webtest/FileServlet.java
+++ src/main/java/com/example/webtest/FileServlet.java
@@ -2,26 +2,35 @@ package com.example.webtest;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 import org.apache.commons.io.IOUtils;
 import org.apache.ibatis.io.Resources;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+// 没有M13g这个注解会上传文件返回500状态码
+@MultipartConfig
 @WebServlet("/file")
 public class FileServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        resp.setContentType("image/png");
-        OutputStream outputStream = resp.getOutputStream();
-        InputStream inputStream = Resources.getResourceAsStream("icon.png");
-        IOUtils.copy(inputStream, outputStream);
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try(FileOutputStream stream = new FileOutputStream("D:\\sonna\\Desktop\\test.png")) {
+            Part part = req.getPart("test-file");
+            IOUtils.copy(part.getInputStream(), stream);
+            resp.setContentType("text/html;charset=UTF-8");
+            resp.getWriter().write("文件上传成功");
+        }
+
     }
 }
